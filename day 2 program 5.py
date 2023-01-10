dc0729885829001c2e51c9e6b9a5b8a840588744
babuvarada <122279705+babuvarada@users.noreply.github.com>
2023-01-10 22:09:18 +0530

    Add files via upload

---
+++ day 2 program 5.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value: "))
+b=int(input("enter the value: "))
+c=int(input("enter the value: "))
+if(c==1):
+        print(a+b)
+if(c==2):
+        print(a-b)
+if(c==3):
+        print(a*b)
+if(c==4):
+        print(a/b)
+
